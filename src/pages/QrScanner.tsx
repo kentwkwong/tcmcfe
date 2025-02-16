b68816c8b1f05c1386a27475c45dd27c9ae94ee0
kentwkwong <kentwkwong@gmail.com>
2025-02-16 14:20:19 -0500

    test cam

--- src/pages/QrScanner.tsx
+++ src/pages/QrScanner.tsx
@@ -49,11 +49,13 @@ import { Html5Qrcode } from "html5-qrcode";
 
 const QRCodeScanner = () => {
   const [cameraType, setCameraType] = useState("environment");
+  const [cameraUsing, setCameraUsing] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [scannedResult, setScannedResult] = useState<string | null>(null);
   const scannerRef = useRef<Html5Qrcode | null>(null);
 
   const stopScanner = () => {
+    setCameraUsing(false);
     if (scannerRef.current) {
       scannerRef.current.stop();
     }
@@ -61,23 +63,25 @@ const QRCodeScanner = () => {
   const startScanner = () => {
     setErrorMessage("");
     setScannedResult(null);
+    setCameraUsing(true);
     const config = { fps: 10, qrbox: 250 }; // Scanner configuration
 
     Html5Qrcode.getCameras()
       .then((devices) => {
         if (devices.length > 0) {
-          const cameraId =
-            devices.find((device) =>
-              cameraType === "environment"
-                ? device.label.includes("back")
-                : device.label.includes("front")
-            )?.id || devices[0].id;
+          console.log(devices);
+          // const cameraId =
+          //   devices.find((device) =>
+          //     cameraType === "environment"
+          //       ? device.label.includes("back")
+          //       : device.label.includes("front")
+          //   )?.id || devices[0].id;
 
           scannerRef.current = new Html5Qrcode("reader");
 
           scannerRef.current
             .start(
-              cameraId,
+              devices[0].id,
               config,
               (decodedText) => {
                 setScannedResult(decodedText);
@@ -110,7 +114,7 @@ const QRCodeScanner = () => {
             checked={cameraType === "environment"}
             onChange={() => setCameraType("environment")}
           />
-          Back Camera
+          Check-In
         </label>
         <label className="ml-4">
           <input
@@ -119,20 +123,28 @@ const QRCodeScanner = () => {
             checked={cameraType === "user"}
             onChange={() => setCameraType("user")}
           />
-          Front Camera
+          Check-Out
         </label>
       </div>
       <button
         onClick={startScanner}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+        className={
+          cameraUsing
+            ? "hidden mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+            : "mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+        }
       >
         Start Scanning
       </button>
       <button
         onClick={stopScanner}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+        className={
+          cameraUsing
+            ? "mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+            : "hidden mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+        }
       >
-        Stop Scanning
+        Cancel
       </button>
       <div id="reader" className="mt-4" />
       {errorMessage && <p className="text-red-500">{errorMessage}</p>}
